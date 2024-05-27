6e292a5a73ebb97c95be310c5053cc39ef699381
kurojuni12 <kurojuni12@gmail.com>
2024-05-28 00:19:52 +0900

    投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,35 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\post;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
-        // getPaginateByLimit()はPost.phpで定義したメソッドです。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    /**
- * 特定IDのpostを表示する
- *
- * @params Object Post // 引数の$postはid=1のPostインスタンス
- * @return Reposnse post view
- */
+
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
- //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
+
     public function create()
     {
         return view('posts.create');
     }
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
         return redirect('/posts/' . $post->id);
     }
 }
